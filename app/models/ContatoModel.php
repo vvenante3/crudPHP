ea92963cc9a9b57e171c2b8948cdca1c09dd6ddb
VitorAcer <vitor.venante@hotmail.com>
2025-08-04 21:05:49 -0300

    preparacao conexao DB [finalizado]

--- app/models/ContatoModel.php
+++ app/models/ContatoModel.php
@@ -10,4 +10,61 @@ class ContatoModel
     {
         $this->db = Database::getInstance();
     }
+
+    // Irá obter todos os contatos
+    public function getAll()
+    {
+        $stmt = $this->db->query("SELECT * FROM contatos ORDER BY id DESC");
+        return $stmt->fetchAll();
+    }
+
+    // Irá obter através do ID de contatos
+    public function getById($id)
+    {
+        $stmt = $this->db->query("SELECT * FROM contatos WHERE id = :id");
+    }
+
+    // Irá inserir um novo contato
+    public function create($data)
+    {
+        $stmt = $this->db->prepare("
+            INSERT INTO contatos (nome, sobrenome, telefone, email)
+            VALUES (:nome, :sobrenome, :telefone, :email)
+        ");
+    
+        return $stmt->execute([
+            'nome'      =>  $data['nome'],
+            'sobrenome' =>  $data['sobrenome'],
+            'telefone'  =>  $data['telefone'],
+            'email'     =>  $data['email'],    
+        ]);
+    }
+
+    // Irá atualizar os contatos já existentes
+    public function update($id, $data)
+    {
+        $stmt = $this->db->prepare("
+            UPDATE CONTATOS SET
+                nome        = :nome,
+                sobrenome   = :sobrenome,
+                telefone    = :telefone,
+                email       = :email
+            WHERE id = :id 
+        ");
+
+        return $stmt->execute([
+            'id'        => $id,
+            'nome'      => $data['nome'],
+            'sobrenome' => $data['sobrenome'],
+            'telefone'  => $data['telefone'],
+            'email'     => $data['email'],
+        ]);
+    }
+
+    // Irá excluir o contato
+    public function delete($id)
+    {
+        $stmt = $this->db->prepare("DELETE FROM contatos WHERE id = :id");
+        return $stmt->execute(['id' => $id]);
+    }
 }
